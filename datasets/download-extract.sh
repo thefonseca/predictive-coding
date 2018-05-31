731d741e5ecea73759c2a97b528e0a2d81067c32
Marcio Fonseca <thefonseca@gmail.com>
2018-05-31 21:09:17 +0100

    Change download script to use wget
    Using wget we can resume interrupted downloads, which is harder to implement using a pure python implementation.

--- datasets/download-extract.sh
+++
@@ -1,1 +0,0 @@
-python download.py http://data.csail.mit.edu/soundnet/actions3/split1/Moments_in_Time_Mini.zip --unzip
---
+++ datasets/moments-download.sh
@@ -0,0 +1,9 @@
+#python download.py http://data.csail.mit.edu/soundnet/actions3/split1/Moments_in_Time_Mini.zip --unzip
+
+# Moments in Time Mini training and validation set
+wget -c -nc http://data.csail.mit.edu/soundnet/actions3/split1/Moments_in_Time_Mini.zip
+unzip -n Moments_in_Time_Mini -d Moments_in_Time_Mini
+
+# Moments in Time Mini test set
+wget -c -nc http://data.csail.mit.edu/soundnet/actions3/split1/momentsMiniTest.zip
+unzip -n momentsMiniTest -d momentsMiniTest
