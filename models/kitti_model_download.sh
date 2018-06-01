7a12786601769cca91aebf39dd661b090b7c50d7
Marcio Fonseca <thefonseca@gmail.com>
2018-06-01 16:50:56 +0100

    Code for PredNet model
    This is an adaptation of the PredNet implementation provided by Lotter et al.

---
+++ models/kitti_model_download.sh
@@ -0,0 +1,5 @@
+savedir="./model_data/model_data_keras2"
+mkdir -p -- "$savedir"
+wget https://www.dropbox.com/s/z7ittwfxa5css7a/model_data_keras2.zip?dl=0 -O $savedir/model_data_keras2.zip
+unzip -j $savedir/model_data_keras2.zip -d $savedir
+rm $savedir/model_data_keras2.zip
