eff66824b9660ec77fd3e844e950003c9c2f96cc
Marcio Fernandes Fonseca <s1780875@tambo.inf.ed.ac.uk>
2018-06-01 14:39:28 +0100

    Add commands to to organize Moments in Time dataset directories.

--- datasets/moments_download.sh
+++ datasets/moments_download.sh
@@ -2,8 +2,24 @@
 
 # Moments in Time Mini training and validation set
 wget -c -nc http://data.csail.mit.edu/soundnet/actions3/split1/Moments_in_Time_Mini.zip
-unzip -n Moments_in_Time_Mini -d moments_data
+
+if [ -e moments_data ]
+then
+    echo "Could not unzip: directory 'moments_data' already exists!"
+else
+    unzip -n Moments_in_Time_Mini
+    echo "Renaming dataset directory to 'moments_data'..."
+    mv ./Moments_in_Time_Mini ./moments_data
+fi
 
 # Moments in Time Mini test set
 wget -c -nc http://data.csail.mit.edu/soundnet/actions3/split1/momentsMiniTest.zip
-unzip -n momentsMiniTest -d moments_test
+
+if [ -e moments_data/test ]
+then
+    echo "Could not unzip: directory 'moments_data/test' already exists!"
+else
+    unzip -n momentsMiniTest
+    echo "Moving test set to directory 'moments_data'..."
+    mv ./momentsMiniTest ./moments_data/test
+fi
