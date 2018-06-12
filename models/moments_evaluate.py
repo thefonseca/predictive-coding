3fd20afe9664c248b44ba654d7f81b0e6326be62
Marcio Fonseca <thefonseca@gmail.com>
2018-06-12 20:24:48 +0100

    Defining experiment parameters in separate script

--- models/moments_evaluate.py
+++ models/moments_evaluate.py
@@ -10,27 +10,33 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import math
 
 from keras import backend as K
 from keras.models import Model, model_from_json
 from keras.layers import Input, Dense, Flatten
+from keras.utils import multi_gpu_model
 
 from prednet import PredNet
 from moments_data import SequenceGenerator
-from moments_settings import *
+from moments_settings import experiments
+
 from tqdm import tqdm
+import argparse
+
+FLAGS = None
 
 
-def load_model(json_file, weights_file):
+def load_model(model_json_file, model_weights_file, **extras):
     # Load trained model
-    f = open(json_file, 'r')
+    f = open(model_json_file, 'r')
     json_string = f.read()
     f.close()
     train_model = model_from_json(json_string, custom_objects = {'PredNet': PredNet})
-    train_model.load_weights(weights_file)
+    train_model.load_weights(model_weights_file)
     return train_model
 
-def create_test_model(train_model, output_mode, n_timesteps=10):
+def create_test_model(train_model, output_mode, n_timesteps=10, gpus=None, **extras):
     # Create testing model (to output predictions)
     layer_config = train_model.layers[1].get_config()
     layer_config['output_mode'] = output_mode
@@ -41,18 +47,22 @@ def create_test_model(train_model, output_mode, n_timesteps=10):
     inputs = Input(shape=tuple(input_shape))
     predictions = test_prednet(inputs)
     test_model = Model(inputs=inputs, outputs=predictions)
+    
+    if gpus:
+        test_model = multi_gpu_model(test_model, gpus=gpus)
+        
     return test_model
 
-def save_predictions(save_dir, output_mode, X, X_hat, 
-                     n_plot=20, experiment_name='exp'):
+def save_predictions(X, X_hat, experiment_name, results_dir, output_mode,
+                     n_plot=20, **extras):
     
     # Compare MSE of PredNet predictions vs. using last frame.  Write results to prediction_scores.txt
     mse_model = np.mean((X[:, 1:] - X_hat[:, 1:]) ** 2)  # look at all timesteps except the first
     mse_prev = np.mean((X[:, :-1] - X[:, 1:]) ** 2)
     
-    save_dir = os.path.join(save_dir, experiment_name + '_' + output_mode)
-    if not os.path.exists(save_dir): os.makedirs(save_dir)
-    f = open(os.path.join(save_dir, 'prediction_scores.txt'), 'w')
+    results_dir = os.path.join(results_dir, experiment_name + '_' + output_mode)
+    if not os.path.exists(results_dir): os.makedirs(results_dir)
+    f = open(os.path.join(results_dir, 'prediction_scores.txt'), 'w')
     f.write("Model MSE: %f\n" % mse_model)
     f.write("Previous Frame MSE: %f" % mse_prev)
     f.close()
@@ -63,7 +73,7 @@ def save_predictions(save_dir, output_mode, X, X_hat,
     plt.figure(figsize = (n_timesteps, 2 * aspect_ratio))
     gs = gridspec.GridSpec(2, n_timesteps)
     gs.update(wspace=0., hspace=0.)
-    plot_save_dir = os.path.join(save_dir, 'prediction_plots/')
+    plot_save_dir = os.path.join(results_dir, 'prediction_plots/')
     if not os.path.exists(plot_save_dir): os.makedirs(plot_save_dir)
     
     plot_idx = np.random.permutation(X.shape[0])[:n_plot]
@@ -92,13 +102,15 @@ def save_predictions(save_dir, output_mode, X, X_hat,
 def evaluate(img_dir, img_sources, output_mode, n_timesteps=10, 
              frame_step=3, seq_overlap=5, max_seq_per_video=5, 
              shuffle=False, batch_size=5, max_missing_frames=15, 
-             seed=17, data_format=K.image_data_format()):
+             N_seq=None, seed=17, data_format=K.image_data_format(), 
+             **extras):
     
     print('Creating generator...')
     test_generator = SequenceGenerator(img_dir, img_sources, n_timesteps,
                                        frame_step=frame_step, seq_overlap=5, 
-                                       max_seq_per_video=max_seq_per_video,
-                                       shuffle=shuffle, batch_size=batch_size,
+                                       max_seq_per_video=max_seq_per_video, 
+                                       N_seq=N_seq, shuffle=shuffle, 
+                                       batch_size=batch_size,
                                        max_missing_frames=max_missing_frames, 
                                        seed=seed, data_format=data_format)
     
@@ -109,7 +121,9 @@ def evaluate(img_dir, img_sources, output_mode, n_timesteps=10,
     #mse_model = 0
     #mse_prev = 0
     
-    n_batches = len(test_generator.possible_starts) / batch_size
+    n_batches = ((len(test_generator.possible_starts) - 1) // batch_size) + 1 # ceil
+    print('Number of sequences: {}'.format(len(test_generator.possible_starts)))
+    print('Number of batches: {}'.format(n_batches))
 
     for i in tqdm(range(n_batches)):
         X_, y = next(test_generator)
@@ -124,13 +138,8 @@ def evaluate(img_dir, img_sources, output_mode, n_timesteps=10,
 
         n += 1
 
-        #if n > 2:
-        #    break
-
     X = np.array(X)
     X_hat = np.array(X_hat)
-    #X_test = np.reshape(X_test, (-1,) + X_test.shape[2:])
-    #X_hat = np.reshape(X_hat, (-1,) + X_hat.shape[2:])
 
     if data_format == 'channels_first':
         X = np.transpose(X, (0, 1, 3, 4, 2))
@@ -143,34 +152,22 @@ def evaluate(img_dir, img_sources, output_mode, n_timesteps=10,
     
     
 if __name__ == '__main__':
-    #parser = argparse.ArgumentParser(description='Evaluate pre-trained model.')
-    #parser.add_argument('config', help='experiment config name defined in ')
-    #FLAGS, unparsed = parser.parse_known_args()
+    parser = argparse.ArgumentParser(description='Evaluate pre-trained model.')
+    parser.add_argument('config', help='experiment config name defined in moments_setting.py')
+    FLAGS, unparsed = parser.parse_known_args()
     
-    #TODO: store configuration in separate file
-    
-    output_mode = 'prediction' # 'prediction' | 'error' | 'features'
-    n_timesteps = 10
-    
-    weights_file = os.path.join(WEIGHTS_DIR, 'prednet_kitti_weights.hdf5')
-    json_file = os.path.join(WEIGHTS_DIR, 'prednet_kitti_model.json')
+    experiment = experiments[FLAGS.config]
+    print('\n==> Starting experiment: {}\n'.format(experiment['description']))
     
     print('Loading pre-trained model...')
-    pretrained_model = load_model(json_file, weights_file)
+    pretrained_model = load_model(**experiment)
     layer_config = pretrained_model.layers[1].get_config()
     data_format = layer_config['data_format'] if 'data_format' in layer_config else layer_config['dim_ordering']
     
     print('Creating testing model...')
-    test_model = create_test_model(pretrained_model, output_mode, n_timesteps=n_timesteps)
+    test_model = create_test_model(pretrained_model, **experiment)
     
-    img_dir = os.path.join(DATA_DIR, 'test')
-    img_sources = os.path.join(DATA_DIR, 'sources_test.pkl')
+    X, X_hat = evaluate(data_format=data_format, **experiment)
     
-    X, X_hat = evaluate(img_dir, img_sources, output_mode, n_timesteps=n_timesteps, 
-                        frame_step=3, seq_overlap=5, max_seq_per_video=5, 
-                        shuffle=False, batch_size=5, max_missing_frames=15, 
-                        seed=17, data_format=data_format)
-    
-    if output_mode == 'prediction':
-        save_predictions(RESULTS_SAVE_DIR, output_mode, X, X_hat, 
-                         n_plot=20, experiment_name='moments')
+    if experiment['output_mode'] == 'prediction':
+        save_predictions(X, X_hat, FLAGS.config, **experiment)
--- models/moments_settings.py
+++ models/moments_settings.py
@@ -1,10 +1,41 @@
+import os
+
 # Where KITTI data will be saved if you run process_kitti.py
 # If you directly download the processed data, change to the path of the data.
 DATA_DIR = '../datasets/moments_data_frames/'
 
-# Where model weights and config will be saved if you run kitti_train.py
-# If you directly download the trained weights, change to appropriate path.
-WEIGHTS_DIR = './model_data/kitti_keras/'
-
 # Where results (prediction plots and evaluation file) will be saved.
-RESULTS_SAVE_DIR = './results/'
+# RESULTS_SAVE_DIR = './results/'
+
+#WEIGHTS_DIR = './model_data/kitti_keras/'
+
+experiments = {
+    'moments_transfer_kitti': {
+        'description': 'Using PredNet pre-trained on KITTI dataset to perform inference \
+on Moments in Time dataset.',
+        'output_mode': 'prediction', # 'prediction' | 'error' | 'features'
+        'n_timesteps': 10,
+        'frame_step': 3,
+        'seq_overlap': 5,
+        'max_seq_per_video': 5,
+        'batch_size': 5,
+        'max_missing_frames': 15,
+        'N_seq': 2,
+        'shuffle': False,
+        'seed': 17,
+        #'gpus': 4,
+        
+        # WEIGHTS
+        'model_weights_file': './model_data/kitti_keras/prednet_kitti_weights.hdf5',
+        'model_json_file': './model_data/kitti_keras/prednet_kitti_model.json',
+        
+        # DATA
+        'img_dir': os.path.join(DATA_DIR, 'test'),
+        'img_sources': os.path.join(DATA_DIR, 'sources_test.pkl'),
+        
+        # RESULTS
+        'results_dir': './results/',
+        'n_plot': 20
+    }
+    
+}
