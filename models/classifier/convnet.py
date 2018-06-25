8bb75cbd668b73391f4b25d224cfc65be9fb25bd
Marcio Fonseca <thefonseca@gmail.com>
2018-06-25 21:48:09 +0100

    Refactoring convnet experiments

--- models/classifier/convnet.py
+++ models/classifier/convnet.py
@@ -1,9 +1,9 @@
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, ConvLSTM2D, Conv3D
 from keras.layers import Activation, Dropout, Flatten, Dense, BatchNormalization
 from keras import backend as K
-from keras.callbacks import ModelCheckpoint, CSVLogger
+from keras.callbacks import ModelCheckpoint, CSVLogger, EarlyStopping
 from keras.models import load_model
 
 from settings import configs
@@ -13,8 +13,7 @@ import argparse
 import utils
 
 
-def get_model(input_shape, n_classes, drop_rate=0.25):
-    
+def convnet(input_shape, n_classes, drop_rate=0.5):
     if K.image_data_format() == 'channels_first':
         input_shape = (input_shape[2], input_shape[0], input_shape[1])
     
@@ -38,46 +37,109 @@ def get_model(input_shape, n_classes, drop_rate=0.25):
     model.add(Dropout(drop_rate))
     model.add(Dense(n_classes))
     model.add(Activation('softmax'))
+    return model
+
+def convlstm(input_shape, n_classes, drop_rate=0.5):
+    if K.image_data_format() == 'channels_first':
+        input_shape = (input_shape[0], input_shape[3], input_shape[1], input_shape[2])
     
+    model = Sequential()
+    model.add(ConvLSTM2D(filters=10, kernel_size=(3, 3),
+                       input_shape=input_shape,
+                       padding='same', return_sequences=True))
+    #model.add(BatchNormalization())
+
+    #seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
+    #                   padding='same', return_sequences=True))
+    #seq.add(BatchNormalization())
+
+    model.add(Conv3D(filters=1, kernel_size=(3, 3, 3),
+                     activation='sigmoid',
+                     padding='same', data_format='channels_last'))
+    
+    model.add(Flatten())
+    model.add(Dense(32))
+    #model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(drop_rate))
+    model.add(Dense(n_classes))
+    model.add(Activation('softmax'))
+    return model
+
+
+def lstm(input_shape, n_classes, drop_rate=0.5):
+    """Build a simple LSTM network. We pass the extracted features from
+    our CNN to this model predomenently."""
+    # Model.
+    model = Sequential()
+    model.add(Flatten(input_shape=self.input_shape))
+    model.add(LSTM(32, return_sequences=False, dropout=drop_rate))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(drop_rate))
+    model.add(Dense(n_classes, activation='softmax'))
+
+
+def save_experiment_config(config_name, base_results_dir, config):
+    results_dir = utils.get_create_results_dir(config_name, base_results_dir)
+    f = open(os.path.join(results_dir, 'experiment_config.txt'), 'w')
+    
+    for key in sorted(config):
+        f.write('{}: {}\n'.format(key, config[key]))
+        
+    f.close()
+
+    
+def train(config_name, training_data_dir, validation_data_dir, 
+          base_results_dir, test_data_dir=None, epochs=10, 
+          use_multiprocessing=False, workers=1, dropout=0.5, 
+          seq_length=None, batch_size=10, stopping_patience=0, 
+          classes=None, **config):
+    
+    max_per_class = config.get('training_max_per_class', None)
+    train_generator = DataGenerator(batch_size=batch_size,
+                                    classes=classes,
+                                    seq_length=seq_length,
+                                    max_per_class=max_per_class)
+    
+    val_generator = DataGenerator(classes=classes,
+                                  seq_length=seq_length,
+                                  batch_size=batch_size)
+    
+    train_generator = train_generator.flow_from_directory(training_data_dir)
+    val_generator = val_generator.flow_from_directory(validation_data_dir)
+    
+    input_shape = train_generator.data_shape
+    n_classes = train_generator.n_classes
+    results_dir = utils.get_create_results_dir(config_name, base_results_dir)
+    
+    if seq_length:
+        model = convlstm(input_shape, n_classes, drop_rate=dropout)
+        checkpoint_path = os.path.join(results_dir, 'convlstm.hdf5')
+        csv_path = os.path.join(results_dir, 'convnet.log')
+    else:
+        model = convnet(input_shape, n_classes, drop_rate=dropout)
+        checkpoint_path = os.path.join(results_dir, 'convnet.hdf5')    
+        csv_path = os.path.join(results_dir, 'convnet.log')
+        
     model.compile(loss='categorical_crossentropy',
                   optimizer='rmsprop',
                   metrics=['accuracy'])
     model.summary()
-    return model
-    
-def train(config_name, training_data_dir, validation_data_dir, 
-          base_results_dir, test_data_dir=None, epochs=10, 
-          use_multiprocessing=False, workers=1,
-          batch_size=10, classes=None, **config):
-    
-    max_per_class = config.get('training_max_per_class', None)
-    training_generator = DataGenerator(batch_size=batch_size,
-                                       classes=classes,
-                                       max_per_class=max_per_class)
-    
-    validation_generator = DataGenerator(classes=classes,
-                                         batch_size=batch_size)
-    
-    training_generator = training_generator.flow_from_directory(training_data_dir)
-    validation_generator = validation_generator.flow_from_directory(validation_data_dir)
-    
-    model = get_model(training_generator.data_shape, 
-                      training_generator.n_classes)
-    
-    results_dir = utils.get_create_results_dir(config_name, base_results_dir)
-    checkpoint_path = os.path.join(results_dir, 'convnet.hdf5')
         
     checkpointer = ModelCheckpoint(filepath=checkpoint_path, 
                                    verbose=1, save_best_only=True)
-    csv_path = os.path.join(results_dir, 'convnet.log')
-    csv_logger = CSVLogger(csv_path)
     
-    model.fit_generator(training_generator,
-                        len(training_generator),
+    csv_logger = CSVLogger(csv_path)
+    stopper = EarlyStopping(monitor='val_loss', 
+                            patience=stopping_patience, 
+                            verbose=0, mode='auto')
+    
+    model.fit_generator(train_generator,
+                        len(train_generator),
                         epochs=epochs,
-                        validation_data=validation_generator,
-                        validation_steps=len(validation_generator),
-                        callbacks=[checkpointer, csv_logger],
+                        validation_data=val_generator,
+                        validation_steps=len(val_generator),
+                        callbacks=[checkpointer, csv_logger, stopper],
                         use_multiprocessing=use_multiprocessing, 
                         workers=workers)
     
@@ -85,12 +147,13 @@ def train(config_name, training_data_dir, validation_data_dir,
 def evaluate(config_name, test_data_dir, batch_size, 
              index_start, base_results_dir, classes=None,
              workers=1, use_multiprocessing=False, 
-             seq_length=20, **config):
+             seq_length=None, **config):
     
     print('\nEvaluating model on test set...')
     # we use the remaining part of training set as test set
     generator = DataGenerator(classes=classes,
-                              batch_size=batch_size, 
+                              batch_size=batch_size,
+                              seq_length=seq_length,
                               index_start=index_start)
     generator = generator.flow_from_directory(test_data_dir)
     
@@ -117,8 +180,9 @@ if __name__ == '__main__':
     config = configs[FLAGS.config]
     print('\n==> Starting traininig: {}'.format(config['description']))
     
-    #train(FLAGS.config, **config)
+    train(FLAGS.config, **config)
     
     if config['test_data_dir']:
         evaluate(FLAGS.config, index_start=config['training_max_per_class'], **config)
 
+    save_experiment_config(FLAGS.config, config['base_results_dir'], config)
--- models/classifier/settings.py
+++ models/classifier/settings.py
@@ -1,62 +1,66 @@
-EMBEDDINGS_PER_VIDEO = 5
 
-configs = {
-    'prednet_kitti__moments_2c__R3': {
-        'description': 'A convnet classifier trained on the R3 PredNet model \
-(pretrained on KITTI) features extracted from the Moments in Time dataset (binary version).',
-        'epochs': 5,
-        'batch_size': 20,
-        'shuffle': True,
-        'seed': 17,
-        'workers': 8,
-        #'use_multiprocessing': True,
-        #'gpus': 4,
+def add_config(configs, name, config, base_config=None):
+    new_config = dict()
+    if base_config:
+        new_config.update(base_config)
         
-        # WEIGHTS
-        #'model_weights_file': './model_data/kitti_keras/prednet_kitti_weights.hdf5',
-        #'model_json_file': './model_data/kitti_keras/prednet_kitti_model.json',
-        
-        # DATA
-        'training_data_dir': '../prednet/results/moments_2c_transfer_kitti_R3/training',
-        'validation_data_dir': '../prednet/results/moments_2c_transfer_kitti_R3/validation',
-        'test_data_dir': '../prednet/results/moments_2c_transfer_kitti_R3/training',
-        'training_max_per_class': 400 * EMBEDDINGS_PER_VIDEO,
-        
-        # RESULTS
-        'base_results_dir': './results/'
-    },
-    
-    'vgg__moments_nano__features' : {
-        'description': 'Extract features from Moments in Time dataset',
-        'input_shape': (160, 160, 3),
-        'batch_size': 10,
-        'frames_per_video': 90,
-        'max_videos_per_class': 200,
-        'sample_step': 3,
-        'base_results_dir': './results',
-        'training_data_dir': '../../datasets/moments_nano_frames/training',
-        'validation_data_dir': '../../datasets/moments_nano_frames/validation',
-        'classes': ['running', 'walking']
-    },
-    
-    'convnet__moments_nano' : {
-        'description': 'A convnet classifier using VGG features',
-        'epochs': 20,
-        'stopping_patience': 5,
-        'batch_size': 20,
-        'seq_length': 20,
-        'shuffle': True,
-        'dropout': 0.5,
-        'seed': 17,
-        'workers': 8,
-        'use_multiprocessing': True,
-        'training_max_per_class': 30 * 100, # features_per_video * max_videos_per_class
-        'base_results_dir': './results',
-        'training_data_dir': './results/vgg__moments_nano__features/training',
-        'validation_data_dir': './results/vgg__moments_nano__features/validation',
-        'test_data_dir': './results/vgg__moments_nano__features/training',
-        'classes': ['cooking', 'walking']
-    }
-    
+    new_config.update(config)
+    configs[name] = new_config
+
+configs = {}
+
+configs['vgg__moments_nano__features'] = {
+    'description': 'Extract features from Moments in Time dataset',
+    'input_shape': (160, 160, 3),
+    'batch_size': 10,
+    'frames_per_video': 90,
+    'max_videos_per_class': 200,
+    'sample_step': 3,
+    'base_results_dir': './results',
+    'training_data_dir': '../../datasets/moments_nano_frames/training',
+    'validation_data_dir': '../../datasets/moments_nano_frames/validation',
 }
 
+VGG_FEATURES_PER_VIDEO = 30
+
+convnet_base_config = {
+    'epochs': 20,
+    'stopping_patience': 5,
+    'batch_size': 20,
+    'shuffle': True,
+    'dropout': 0.5,
+    'seed': 17,
+    'workers': 4,
+    #'use_multiprocessing': True,
+    'training_max_per_class': VGG_FEATURES_PER_VIDEO * 100, # features_per_video * max_videos_per_class
+    'base_results_dir': './results',
+    'training_data_dir': './results/vgg__moments_nano__features/training',
+    'validation_data_dir': './results/vgg__moments_nano__features/validation',
+    'test_data_dir': './results/vgg__moments_nano__features/training',
+}
+    
+add_config(configs, 'convnet__moments_nano__vgg_features_easy', 
+           { 'description': 'A convnet classifier using VGG features',
+             'classes': ['cooking', 'walking']}, convnet_base_config)
+
+add_config(configs, 'convnet__moments_nano__vgg_features_hard', 
+           { 'description': 'A convnet classifier using VGG features',
+             'classes': ['running', 'walking']}, convnet_base_config)
+
+add_config(configs, 'convlstm__moments_nano__vgg_features_easy', 
+           { 'description': 'A convnet classifier using VGG features',
+             'seq_length': 30,
+             'classes': ['cooking', 'walking']}, convnet_base_config)
+
+PREDNET_FEATURES_PER_VIDEO = 5
+
+add_config(configs, 'convnet__moments_nano__prednet_kitti_R3_easy', 
+           { 'description': 'A convnet classifier trained on the PredNet \
+(pretrained on KITTI) R3 features extracted from the Moments in Time dataset.',
+             #'model_weights_file': './model_data/kitti_keras/prednet_kitti_weights.hdf5',
+             #'model_json_file': './model_data/kitti_keras/prednet_kitti_model.json',
+             'training_data_dir': '../prednet/results/prednet_kitti__moments_nano__R3/training',
+             'validation_data_dir': '../prednet/results/prednet_kitti__moments_nano__R3/validation',
+             'test_data_dir': '../prednet/results/moments_2c_transfer_kitti_R3/training',
+             'training_max_per_class': PREDNET_FEATURES_PER_VIDEO * 100, # features_per_video * max_videos_per_class
+             'classes': ['cooking', 'walking']}, convnet_base_config)
