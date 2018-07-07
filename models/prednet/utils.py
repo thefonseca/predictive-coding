1f431b4152d9ca049ed5d66e05777f8dca73f213
Marcio Fonseca <thefonseca@gmail.com>
2018-07-07 17:42:43 +0100

    Fix image cropping

--- models/prednet/utils.py
+++ models/prednet/utils.py
@@ -123,18 +123,18 @@ def get_config_str(config):
     return config_str
 
 def crop_center(img, cropx, cropy):
-        y,x,c = img.shape
-        startx = x//2-(cropx//2)
-        starty = y//2-(cropy//2)    
-        return img[startx:startx+cropx,starty:starty+cropy]
+    x,y,c = img.shape
+    startx = x//2-(cropx//2)
+    starty = y//2-(cropy//2)   
+    return img[startx:startx+cropx,starty:starty+cropy]
 
 def resize_img(img, target_size):
-    larger_dim = np.argsort(target_size)[-1]
-    smaller_dim = np.argsort(target_size)[-2]
-    target_ds = float(target_size[larger_dim])/img.shape[larger_dim]
-
-    img = resize(img, (int(np.round(target_ds * img.shape[0])), 
-                       int(np.round(target_ds * img.shape[1]))),
+    ratios = [float(target_size[i]) / img.shape[i] for i in range(len(target_size))]
+    larger_ratio = max(ratios)
+    new_shape = [float(larger_ratio) * img.shape[i] for i in range(len(target_size))]
+    
+    img = resize(img, (int(np.round(new_shape[0])), 
+                       int(np.round(new_shape[1]))),
                  mode='reflect')
     
     # crop
